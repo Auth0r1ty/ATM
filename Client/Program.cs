60ad81edaae9f10e13177e40363acf6d662a3d86
Aleksandar <sale.novakovic403@gmail.com>
2019-12-09 19:05:59 +0100

    added Manager library

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,22 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            /// Define the expected service certificate. 
+            string srvCertCN = "wcfservice";
+
+            /// Define the expected certificate for signing ("<username>_sign" is the expected subject name).
+            /// .NET WindowsIdentity class provides information about Windows user running the given process
+            string signCertCN = String.Empty;
+
+            /// Define subjectName for certificate used for signing which is not as expected by the service
+            string wrongCertCN = String.Empty;
+
+            NetTcpBinding binding = new NetTcpBinding();
+            string address = "net.tcp://localhost:9999/CardService";
+
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
+            
+
         }
     }
 }
